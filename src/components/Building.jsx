3e21c6399d7b8d17c96098848290c42222e4c1a3
Jorge Alvarado <george1996alvarado@gmail.com>
2023-05-12 11:25:47 -0600

    added a new background color feature with conditional statements

--- src/components/Building.jsx
+++ src/components/Building.jsx
@@ -2,9 +2,22 @@ import HeightBar from './HeightBar'
 import Architect from './Architect'
 
 const Building = (props) => {
-  console.log(props.building.architects)
+  const heightRanking = props.building.ranking
+  let buildingColor
+  if (heightRanking <= 10) {
+    buildingColor = 'red'
+  } else if (heightRanking > 10 && heightRanking <= 20) {
+    buildingColor = 'orange'
+  } else if (heightRanking > 20 && heightRanking <= 30) {
+    buildingColor = 'green'
+  } else if (heightRanking > 30 && heightRanking <= 40) {
+    buildingColor = 'blue'
+  } else if (heightRanking > 40) {
+    buildingColor = 'purple'
+  }
+
   return (
-    <div className="building">
+    <div className="building" style={{ backgroundColor: buildingColor }}>
       <img src={props.building.picture} alt={props.building.name}></img>
       <h2>{props.building.name}</h2>
       <h3>{props.building.city},</h3>
